71ccc7ecc80c4ed1830f6d1106352123597fe42e
Sohan-Patra3 <137480481+Sohan-Patra3@users.noreply.github.com>
2024-08-02 11:00:10 +0530

    Update function.js

--- jshindi/functions/function.js
+++ jshindi/functions/function.js
@@ -1,22 +1,54 @@
+//rest operator
 function calculateCartPrice(...num1){
-    return num1;
+    return num1
 }
-console.log(calculateCartPrice(200,400,500));
+
+console.log(calculateCartPrice(20));
+
+function calculate(val1 , val2 , ...num1){
+    return num1
+}
+
+console.log(calculate(12,23,300,400));
+
 
 const user = {
-    name:'sohan',
-    price : 199
+    name : "sohan",
+    age : 24
 }
 
-function handleObject(anyObject){
-    console.log(`username is ${anyObject.name} and price is ${anyObject.price}`);
+function name(user){
+    console.log(`User name is ${user.name} and age is ${user.age}`);
 }
 
-handleObject(user)
+name(user);
 
-const myNewArr = [100,200,400,200]
+const myArr = [100,200,300]
 
-function getArray(myArr){
-    return myArr[1]
+function Array(arr){
+    return arr[1]
 }
-console.log(getArray(myNewArr));
+
+console.log(Array(myArr));
+
+function addTwoNumber(num1 , num2){
+    return num1+num2;
+}
+
+console.log(addTwoNumber(2,3));
+
+const a = addTwoNumber(2,3);
+console.log(a);
+
+function userName(username){
+    if(username===undefined){
+        console.log("please enter your name");
+        return
+    }
+    return `${username} just loggedin`
+}
+
+console.log(userName());
+console.log(userName("sohan"));
+
+
